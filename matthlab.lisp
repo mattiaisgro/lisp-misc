d7ec827f684b0dc69e9a5b73b60303401fe9a34e
Mattia Isgrò <matth_590@yahoo.it>
2017-08-28 10:17:02 +0200

    Add new functions

--- matthlab.lisp
+++ matthlab.lisp
@@ -12,6 +12,25 @@
 
 (defun cube (x) (* x x x))
 
+(defun divides-by? (x y)
+	(if (zerop (mod x y))
+		t
+		nil))
+
+; Execute a procedure with some probability, else execute (other)
+(defun call-probability (f other probability)
+	(if (< random probability)
+		(funcall f)
+		(funcall other)))
+
+(defun average (x y)
+	(/ (+ x y) 2.0))
+
+(defun weighed-average (x xweight y yweight)
+	(/	(+ (* x xweight) (* y yweight))
+		(+ xweight yweight)))
+
+
 ; Calculate x^y (y must be natural)
 (defun pow (x y)
 	(defun pow-iter (res x y counter)
@@ -37,7 +56,7 @@
 			*dx*)))
 
 ; Return a function that approximates the integral of f(x)
-; Using Simpson approximation
+; Using Cavalieri-Simpson approximation
 (defun integrate-f (f)
 	(defun integ-simpson (f a b k res)
 		(if (= k *integral-precision*)
@@ -50,4 +69,11 @@
 		(*
 			(/ (/ (- b a) 3.0) *integral-precision*)
 			(integ-simpson f a b (/ *precision* 100.0) 0.0))))
+
+
+; (defun integrate-f-other (f)
+; 	(defun integ-iter (f a b counter)
+; 		())
+
+; 	(lambda (a b) (integ-iter f a b counter)))
 
